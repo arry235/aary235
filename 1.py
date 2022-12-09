c96ffbdd25bd4035c1d4b11a209150a0f1f5c626
unknown <arry235>
2022-12-09 10:23:34 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,13 @@
+x=5
+y=10
+addition=x+y
+sub= x-y
+mul=x*y
+div=x/y
+
+print("This is the sum:",addition)
+print("This is the sub:",sub)
+print("This is the mul:",mul)
+print("This is the div:",div)
+print("hello world")
+
